3dad1d1f0d52abb2083020c04667b33d15d12286
Anastasia <nastya.elfimova@inbox.ru>
2023-03-28 02:10:43 +0500

    add task 1

---
+++ src/seminar1/Main.java
@@ -0,0 +1,18 @@
+package seminar1;
+
+import java.util.Scanner;
+import seminar1.tasks.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите натуральное число n: ");
+        String n = iScanner.next();
+        iScanner.close();
+
+        int numb = Integer.parseInt(n);
+        int res = Task1.calculateTriangularNumber(numb);
+
+        System.out.println("n-ое треуг.число: " + res);
+    }
+}
